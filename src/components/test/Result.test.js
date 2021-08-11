02f5763f33b126c2317b304364d1ba6ca898988e
awwadsaeed <saeedawwad450@gmail.com>
2021-08-11 14:58:22 +0300

    fixes

--- src/components/test/Result.test.js
+++ src/components/test/Result.test.js
@@ -17,22 +17,3 @@ it('Should render results', () => {
     const items = screen.getByTestId('results');
     expect(items).toHaveTextContent('{ "Headers": { "content-type": "string application/json" }, "count": 2, "results": [ { "name": "fake thing 1", "url": "http://fakethings.com/1" }, { "name": "fake thing 2", "url": "http://fakethings.com/2" } ] }');
 });
-// expect(items,results).toHaveLength(2);
-//     expect(items).toContainHTML(
-//         `<pre data-testid="renderedData">{
-//   "Headers": {
-//     "content-type": "string application/json"
-//   },
-//   "count": 2,
-//   "results": [
-//     {
-//       "name": "fake thing 1",
-//       "url": "http://fakethings.com/1"
-//     },
-//     {
-//       "name": "fake thing 2",
-//       "url": "http://fakethings.com/2"
-//     }
-//   ]
-// }</pre>`);
-// expect(items[1]).toHaveTextContent('Darth Vader');
